80826afa1071ba36ee2ce0266d1d5f54f54cc6c6
Dayanara Gonzalez Gomez <dayanaragonzalez@wifi-roaming-128-4-158-61.wifi.udel.edu>
2025-10-20 21:56:47 -0400

    Fixed login URL and session logic

--- static/script.js
+++ static/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const password = document.getElementById("password").value;
 
       try {
-        const response = await fetch("http://127.0.0.1:5001/api/login", {
+        const response = await fetch("/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ username, password }),
